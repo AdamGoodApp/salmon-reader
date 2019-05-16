dd46e3935c482b8f5c5a495a567f177c5c4deada
adamgoodapp <adamgoodapp@gmail.com>
2019-05-17 00:06:03 +0900

    Add material

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/home";
+
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
       </header>
+
+      <Home />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/home/index.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Home extends Component<any, any> {
+  render() {
+    return <div className="home-container">Hello from Home</div>;
+  }
+}
+
+export default Home;
